f23c29e92223ec7784dce418782307b7b7b9db74
JettyJiang <junjie.jiang@hotmail.com>
2020-12-08 11:35:46 +0800

    fix

--- src/android/BluetoothLePlugin.java
+++ src/android/BluetoothLePlugin.java
@@ -3010,13 +3010,13 @@ public class BluetoothLePlugin extends CordovaPlugin {
           }
           addProperty(returnObj, keyUuid, uuidsString);
           // addPropertyBytes(returnObj, keyAdvertisement, result.getScanRecord().getManufacturerSpecificData(0x004c));
-          byte[] bytes = null;
-          bytes = result.getScanRecord().getManufacturerSpecificData(0x004c);
-          if( bytes != null){
-            addPropertyBytes(returnObj, keyAdvertisementManu, bytes);
-            // String str = new String(bytes, StandardCharsets.UTF_8);
-            // addProperty(returnObj, keyAdvertisementManu, str);
-          }
+          // byte[] bytes = null;
+          // bytes = result.getScanRecord().getManufacturerSpecificData(0x004c);
+          // if( bytes != null){
+          //   addPropertyBytes(returnObj, keyAdvertisementManu, bytes);
+          //   // String str = new String(bytes, StandardCharsets.UTF_8);
+          //   // addProperty(returnObj, keyAdvertisementManu, str);
+          // }
           addProperty(returnObj, keyStatus, statusScanResult);
 
           int txPowerLevelScan = result.getScanRecord().getTxPowerLevel();
